4935513921f175d4ed79b986e909a6b1e789415b
Marcos Torres <towers1337@gmail.com>
2026-02-01 22:23:32 -0500

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -1,12 +1,1 @@
-;;; early-init.el --- Maximum Speed Pre-Boot
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
 
-;; Disable UI instantly to prevent rendering lag
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars . nil) default-frame-alist)
-(setq-default frame-inhibit-implied-resize t)
-
-;; Prevent package.el from slowing down startup
-(setq package-enable-at-startup nil)
